8545022cd151733b01cf4ebdb9bd31ef8b6264e6
Trianabot <45466002+Trianabot@users.noreply.github.com>
2019-12-18 16:46:50 +0530

    Update config.js

--- config/config.js
+++ config/config.js
@@ -2,5 +2,5 @@ module.exports = {
     jwtSecret: 'Trinabot',
     db:'mongodb://studapp:studapp22@ds157857.mlab.com:57857/studentapp',
    port: 2210,
-     redirectUrl: 'localhost:4200',
+     redirectUrl: 'ec2-3-231-255-189.compute-1.amazonaws.com:4200',
 };
